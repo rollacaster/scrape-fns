af70a0b5ba0d2f98c48d96b17393dd9d9a8cd590
Thomas Sojka <thomas.sojka@comsysto.com>
2019-12-07 18:17:43 +0100

    Change stuff

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-const fs = require('fs')
 const axios = require('axios')
 const jsdom = require('jsdom')
 const { fromPromise } = require('crocks/Async')
-const { curry, binary, map, compose, prop } = require('crocks')
+const { map, compose, prop } = require('crocks')
 
 const { JSDOM } = jsdom
 
-exports.writeFileSync = curry(binary(fs.writeFileSync))
-
-exports.nodeListMap = f => nodeList => {
-  const result = []
-  for (let element of nodeList.values()) {
-    result.push(f(element))
-  }
-  return result
-}
-
-exports.$ = selector => html =>
-  new JSDOM(html).window.document.querySelectorAll(selector)
+exports.$ = selector =>
+  compose(Array.from, html =>
+    new JSDOM(html).window.document.querySelectorAll(selector)
+  )
 
 exports.get = url =>
   compose(map(prop('data')), fromPromise(() => axios.get(url)))
